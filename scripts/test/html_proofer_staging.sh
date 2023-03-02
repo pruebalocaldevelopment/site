cee22a152efb5b117c0dd74faf2aadddc82d7e2e
SandraEliz <117297044+SandraEliz@users.noreply.github.com>
2023-03-02 11:00:07 -0600

    Update html_proofer_staging.sh

--- scripts/test/html_proofer_staging.sh
+++ scripts/test/html_proofer_staging.sh
@@ -4,12 +4,12 @@
 
 # Figure out the Jekyll baseurl. If this doesn't work for some reason, you can
 # just hardcode it, eg: JEKYLL_BASEURL="/my-baseurl"
-JEKYLL_BASEURL=/
-#$GITHUB_REPOSITORY_NAME_PART
+JEKYLL_BASEURL=/$GITHUB_REPOSITORY_NAME_PART
 # We have to create a temporary folder to test in, because html-proofer does not
 # like Jekyll's "baseurl", and interprets most links as broken.
 mkdir -p ./_test$JEKYLL_BASEURL &&
-cp -r ./_site/* ./_test$JEKYLL_BASEURL/ &&
+cp -r ./* ./_test$JEKYLL_BASEURL/ &&
 touch ./_test/index.html &&
 bundle exec htmlproofer --allow-hash-href --disable-external ./_test &&
 rm -rf ./_test
+#_site/
